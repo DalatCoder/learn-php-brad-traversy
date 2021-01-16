44782c316f4badec8f0806056cda52a5e35c22ae
Trong Hieu <hieuntctk42@gmail.com>
2021-01-16 20:39:22 +0700

    Add config & autoload libraries

---
+++ traversymvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// App Root
+define('APP_ROOT' ,dirname(dirname(__FILE__)));
+
+// URL Root
+define('URL_ROOT', 'http://localhost/php-oop-mvc/traversymvc');
+
+// Site Name
+define('SITE_NAME', 'TraversyMVC');
--- traversymvc/app/views/pages/index.php
+++ traversymvc/app/views/pages/index.php
@@ -1,3 +1,5 @@
 <h1><?php if (isset($data)) {
         echo $data['title'];
     } ?></h1>
+
+<?php echo APP_ROOT ?>
