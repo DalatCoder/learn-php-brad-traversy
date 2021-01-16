57475c0a349ff8a399be0a6801c71a8dd11ecb97
Trong Hieu <hieuntctk42@gmail.com>
2021-01-16 17:11:57 +0700

    Constructor and Destructor

--- pre-course/oop/user.php
+++ pre-course/oop/user.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 class User
 {
     public $name;
@@ -14,4 +15,29 @@ $user1 = new User();
 echo $user1->name;
 echo '<br>';
 echo $user1->sayHello();
+*/
 
+class User
+{
+    public $name;
+    public $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function __destruct()
+    {
+        echo 'Destructor ran...';
+    }
+
+    public function sayHello()
+    {
+        return $this->name . ' says hello';
+    }
+}
+
+$user1 = new User('Brad', 19);
+var_dump($user1);
