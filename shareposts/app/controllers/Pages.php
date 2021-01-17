fbdd23fb1313c19711cf094af146345dcdccd4bb
Trong Hieu <hieuntctk42@gmail.com>
2021-01-17 18:55:42 +0700

    Redirect user to posts page when user is logged in

--- shareposts/app/controllers/Pages.php
+++ shareposts/app/controllers/Pages.php
@@ -9,6 +9,10 @@ class Pages extends Controller
 
     public function index()
     {
+        if (isLoggedIn()) {
+            redirect('posts');
+        }
+
         $data = [
             'title' => 'Share Posts',
             'description' => 'Simple social network built on the TraversyMVC PHP framework'
