e0b4a895b1e668cb6a80fae0ac396b1052c351c4
Trong Hieu <hieuntctk42@gmail.com>
2021-01-17 14:37:35 +0700

    Validating input 🚀🚀🚀

--- shareposts/app/controllers/Users.php
+++ shareposts/app/controllers/Users.php
@@ -12,6 +12,54 @@ class Users extends Controller
         // Check for POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
+
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            ];
+
+            // print_r($data);
+
+            // Validate Email
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            // Validate Name
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter name';
+            }
+
+            // Password
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+            // Validate Confirm Password
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else {
+                if ($data['password'] != $data['confirm_password']) {
+                    $data['confirm_password_err'] = 'Password do not match';
+                }
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                // Validated
+                die('SUCCESS');
+            }
         } else {
             // Load form
 
@@ -26,10 +74,10 @@ class Users extends Controller
                 'password_err' => '',
                 'confirm_password_err' => ''
             ];
-
-            // Load view
-            $this->view('users/register', $data);
         }
+
+        // Load view
+        $this->view('users/register', $data);
     }
 
     public function login()
@@ -37,6 +85,36 @@ class Users extends Controller
         // Check for POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
+
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            var_dump($data);
+
+            // Validate Email
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            // Password
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err']) && empty($data['password_err'])) {
+                // Validated
+                die('SUCCESS');
+            }
         } else {
             // Load login form
 
@@ -47,9 +125,9 @@ class Users extends Controller
               'email_err' => '',
               'password_err' => ''
             ];
-
-            // Load view
-            $this->view('users/login', $data);
         }
+
+        // Load view
+        $this->view('users/login', $data);
     }
 }
