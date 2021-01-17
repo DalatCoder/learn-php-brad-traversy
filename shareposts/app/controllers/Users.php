d4ab968131d1c4db92c713a0f48ff9e0731ba2f3
Trong Hieu <hieuntctk42@gmail.com>
2021-01-17 17:00:00 +0700

    Store user in session and add logout functionality 🐱‍🏍🐱‍🏍🐱‍🏍

--- shareposts/app/controllers/Users.php
+++ shareposts/app/controllers/Users.php
@@ -133,7 +133,7 @@ class Users extends Controller
                 $loggInUser = $this->userModel->login($data['email'], $data['password']);
                 if ($loggInUser) {
                     // Create Session
-                    die('success');
+                    $this->createUserSession($loggInUser);
                 } else {
                     flash('login_failed', 'Login failed! Incorrect email or password 😢', 'alert alert-danger');
                 }
@@ -152,5 +152,32 @@ class Users extends Controller
 
         // Load view
         $this->view('users/login', $data);
+    }
+
+    public function createUserSession($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_name'] = $user->name;
+        $_SESSION['user_email'] = $user->email;
+
+        redirect('pages/index');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('/users/login');
+    }
+
+    public static function isLoggedIn()
+    {
+        if (isset($_SESSION['user_id'])) {
+            return true;
+        }
+
+        return false;
     }
 }
